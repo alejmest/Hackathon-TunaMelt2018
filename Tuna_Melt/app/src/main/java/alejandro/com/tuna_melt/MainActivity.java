8eaaec43e84b1025c992f099b455646b07494f70
alejmest <ajm162330@utdallas.edu>
2018-02-24 13:53:27 -0600

    updated project file

---
+++ Tuna_Melt/app/src/main/java/alejandro/com/tuna_melt/MainActivity.java
@@ -0,0 +1,13 @@
+package alejandro.com.tuna_melt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
