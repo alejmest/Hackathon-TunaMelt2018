cbd3c25cf67c6ee776d199efc41dcc1677cac387
Gina Fossati <ginarf@comcast.net>
2018-02-24 20:17:25 -0600

    Added melt button passes image to imageprocessor selectionsort

--- Tuna_Melt/app/src/main/java/alejandro/com/tuna_melt/MainActivity.java
+++ Tuna_Melt/app/src/main/java/alejandro/com/tuna_melt/MainActivity.java
@@ -1,5 +1,6 @@
 package alejandro.com.tuna_melt;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,7 +20,7 @@ import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
     ImageView img;
-    Button imgsel;
+    Button imgsel, imgmelt;
     int GALLERY_REQUEST=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +28,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         imgsel = findViewById(R.id.imgsel);
+        imgmelt = findViewById(R.id.imgmelt);
         img = findViewById(R.id.unaltered);
+
+        imgmelt.setVisibility(View.GONE);//make the melt button invisible until an image is selected
         img.setImageResource(R.drawable.ic_launcher_background);
         imgsel.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -39,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        if(imgmelt.isCursorVisible()){
+            imgmelt.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    Bitmap selectedimg = ((BitmapDrawable)img.getDrawable()).getBitmap();
+                    ImageProcessor alteredimg = new ImageProcessor(selectedimg);
+                    Toast.makeText(MainActivity.this, "Image melting...", Toast.LENGTH_SHORT).show();
+                    img.setImageBitmap(alteredimg.selectionSort());
+                }
+            });
+        }
     }
 
     @Override
@@ -53,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 Bitmap selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageURI);
                 Toast.makeText(MainActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
                 img.setImageBitmap(selectedImage);
+                imgmelt.setVisibility(View.VISIBLE);//make the melt button visible
 
             } catch (IOException e) {
                 e.printStackTrace();
