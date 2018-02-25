d255fe815dcec6c5dbe0e43dae58d05368981252
Gina Fossati <ginarf@comcast.net>
2018-02-25 07:11:14 -0600

    Added selection and insertion activities for notes

---
+++ Tuna_Melt/app/src/main/java/alejandro/com/tuna_melt/InsertionActivity.java
@@ -0,0 +1,22 @@
+package alejandro.com.tuna_melt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Gina on 2/25/2018.
+ */
+
+public class InsertionActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_insertion);
+    }
+}
--- Tuna_Melt/app/src/main/java/alejandro/com/tuna_melt/LearnActivity.java
+++ Tuna_Melt/app/src/main/java/alejandro/com/tuna_melt/LearnActivity.java
@@ -1,17 +1,41 @@
 package alejandro.com.tuna_melt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Gina on 2/25/2018.
  */
 
 public class LearnActivity extends AppCompatActivity {
+    private Button selection, insertion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learn_activity);
+
+        selection = findViewById(R.id.selection);
+        insertion = findViewById(R.id.insertion);
+
+        selection.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent goPlay = new Intent(LearnActivity.this, SelectionActivity.class);
+                startActivity(goPlay);
+            }
+        });
+
+
+        insertion.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent goLearn = new Intent(LearnActivity.this, InsertionActivity.class);
+                startActivity(goLearn);
+            }
+        });
+
     }
 
 
