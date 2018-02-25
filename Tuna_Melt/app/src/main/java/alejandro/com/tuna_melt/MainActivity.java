bdc72136f9921e442856c8f49cce4942f0f4cc12
alejmest <ajm162330@utdallas.edu>
2018-02-24 18:12:11 -0600

    Resolved conflicts

--- Tuna_Melt/app/src/main/java/alejandro/com/tuna_melt/MainActivity.java
+++ Tuna_Melt/app/src/main/java/alejandro/com/tuna_melt/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity
 {
 
 
-
-public class MainActivity extends AppCompatActivity {
     ImageView img;
     Button imgsel;
     int GALLERY_REQUEST=-1;
@@ -48,12 +46,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+    protected void onActivityResult(int reqCode, int resultCode, Intent data)
+    {
 
         super.onActivityResult(reqCode, resultCode, data);
 
 
-        if (resultCode == Activity.RESULT_OK && resultCode == GALLERY_REQUEST) {
+        if (resultCode == Activity.RESULT_OK && resultCode == GALLERY_REQUEST)
+        {
             try {
                 final Uri imageURI = data.getData();
                 final InputStream imageStream = getContentResolver().openInputStream(imageURI);
@@ -68,46 +68,19 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
             }
 
-        imgsel = findViewById(R.id.imgsel);
-        img=findViewById(R.id.unaltered);
-        img.setImageResource(R.drawable.ic_launcher_background);
-        imgsel.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
+            imgsel = findViewById(R.id.imgsel);
+            img = findViewById(R.id.unaltered);
+            img.setImageResource(R.drawable.ic_launcher_background);
+            imgsel.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
 
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
-                Toast.makeText(MainActivity.this, "image selected",Toast.LENGTH_LONG).show();
-            }
+                    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                    photoPickerIntent.setType("image/*");
+                    startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+                    Toast.makeText(MainActivity.this, "image selected", Toast.LENGTH_LONG).show();
+                }
 
-        });
-    }
-    @Override
-    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
-
-        super.onActivityResult(reqCode, resultCode, data);
-
-
-        if (resultCode == Activity.RESULT_OK&&resultCode==GALLERY_REQUEST)
-        {
-            try {
-                final Uri imageURI = data.getData();
-                final InputStream imageStream =getContentResolver().openInputStream(imageURI);
-                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-
-
-                img.setImageBitmap(selectedImage);
-
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-            }
-
-        } else {
-            Toast.makeText(MainActivity.this, "you haven't picked an image  "+resultCode+"  "+GALLERY_REQUEST, Toast.LENGTH_LONG).show();
+            });
         }
     }
 }
